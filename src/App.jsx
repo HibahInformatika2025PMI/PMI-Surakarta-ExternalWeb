1e39808b6d2fcbf2e9e0702f7e7bc9c889deedde
IrfanaDipa <irfanadiprasetya@gmail.com>
2025-04-22 00:45:35 +0700

    add new branch and delete files

--- src/App.jsx
+++ src/App.jsx
@@ -23,45 +23,11 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <Navbar />
-
         <main className='bg-[#F7F3F5]'>
           <Routes>
-            {/* Authentication */}
-            <Route path='/auth' element={ <AuthIndex /> } />
-
-            {/* Admin */}
-            {/* Edit Artikel */}
-            <Route path="/admin-news" element={ <AdminNews /> } />
-            <Route path="/admin-news/form" element={ <ArticleForm /> } />
-
-
-            {/* Client */}
-            {/* Beranda Page */}
-            <Route path="/" element={ <Homepage /> } />
-
-            {/* Donor Darah Page */}
-
-            {/* Layanan Ambulance Page */}
-
-            {/* Portal Info Page */}
-
-            {/* Berita Page */}
-            <Route path='/news' element={ <ClientNews /> } />
-            <Route path="/news/article/:id" element={ <ArticleDetails /> } />
-
-            {/* Tentang Kami Page */}
-            <Route path='/about-us' element={ <AboutUs /> } />
-
-            {/* User Profile Pages */}
-            <Route path="/profile" element={ <UserProfile /> } />
-            <Route path="/profile/:userId" element={ <UserProfile /> } />
-            <Route path="/profile/edit/:userId" element={ <EditProfile /> } />
-            <Route path="/profile/edit" element={ <EditProfile /> } />
+            <Route path='/' element={ <Homepage /> } />
           </Routes>
         </main>
-        
-        <Footer />
       </div>
     </Router>
   );
